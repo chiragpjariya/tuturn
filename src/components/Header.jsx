920f685cde27853979abece4fdc8203a6b3f23af
chirag jariya <chiragjariya0109@gmail.com>
2025-03-10 13:47:40 +0530

    new update

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,34 +2,64 @@ import React from 'react'
 import Image from 'next/image';
 import Link from 'next/link';
 
+import Avatar from '@mui/material/Avatar';
+
+
+
+//icons
+import ChatBubbleOutlineOutlinedIcon from '@mui/icons-material/ChatBubbleOutlineOutlined';
+import NotificationsNoneOutlinedIcon from '@mui/icons-material/NotificationsNoneOutlined';
+import KeyboardArrowDownOutlinedIcon from '@mui/icons-material/KeyboardArrowDownOutlined';
+
+
 function Header() {
     return (
-        <nav className=' h-[80px] w-full bg-red-300 flex'>
+        <nav className=' h-[80px] w-full  flex border-b-1 '>
 
             {/* brand */}
             <div className='h-[80px] w-[120px] ml-[12px] flex items-center'>
                 <Image
                     src="/imgs/logo.png"
-                    alt="Example"
+                    alt="logo"
                     width={500}
                     height={300}
                 />
             </div>
 
             {/* nav-manu */}
-            <div className='w-[80%] h-[80px] bg-amber-400 flex justify-end'>
-                <ul className='w-[700px] h-full flex items-center bg-blue-500 justify-between px-2'>
-                    <li>
+            <div className='w-[80%] h-[80px]  flex justify-end'>
+                <ul className='w-[500px] h-full flex items-center  justify-between px-2'>
+                    <li className='h-full flex items-center'>
                         <Link href="#" className='text-[16px] font-sans'> Online classes</Link>
                     </li>
-                    <li>
+                    <li className='h-full flex items-center'>
                         <Link href="#" className='text-[16px] font-sans'>Learning material</Link>
                     </li>
-                    <li>
+                    <li className='h-full flex items-center'>
                         <Link href="#" className='text-[16px] font-sans'>Success stories</Link>
                     </li>
+                    <li className="relative h-full flex items-center before:absolute before:top-0 before:left-0 before:w-full  before:h-[3px] before:bg-orange-500">
+                        <Link href="#" className="text-[16px] font-sans">
+                            Pages
+                            <KeyboardArrowDownOutlinedIcon />
+                        </Link>
+                    </li>
+                </ul>
+            </div>
+
+            {/* icons  */}
+            <div className=' w-[180px] h-[100%] flex  flex-col items-center px-2'>
+                <ul className='w-full h-full flex   items-center justify-evenly'>
                     <li>
-                        <Link href="#" className='text-[16px] font-sans'>Pages</Link>
+                        <Link href="#"><ChatBubbleOutlineOutlinedIcon /> </Link>
+                    </li>
+                    <li>
+                        <Link href="#"><NotificationsNoneOutlinedIcon /> </Link>
+                    </li>
+                    <li>
+                        <Link href="#">
+                            <Avatar alt="Travis Howard" src="/imgs/login.png" />
+                        </Link>
                     </li>
                 </ul>
             </div>
